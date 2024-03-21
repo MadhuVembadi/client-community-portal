76d6502be0b6b2f4ec63e221fd9d4bc21be04147
Madhu Vembadi <vembadi.madhu@gmail.com>
2024-03-21 23:56:15 +0530

    callRecord test

--- APIs/callRecordAPI.js
+++ APIs/callRecordAPI.js
@@ -2,7 +2,7 @@ const exp = require('express');
 const callRecordApp = exp.Router();
 const expressAsyncHandler = require('express-async-handler');
 require('dotenv').config();
-callRecordApp.get('/notificationClient',expressAsyncHandler(async(request,response) => {
+callRecordApp.post('/notificationClient',expressAsyncHandler(async(request,response) => {
     console.log(request);
     response.send(request.body);
 }))
